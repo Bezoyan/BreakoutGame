1661967c99c85a00ddbc70e5077b05a2b02dff04
Bezoyan <zoya1905bezoyan@gmail.com>
2019-05-22 17:39:37 +0400

    add main game screen

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,63 +1,55 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.screens.MainGameScreen;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-//	ShapeRenderer bat;
-	Sprite sprite;
-	float x;
-	float y;
+public class MyGdxGame extends Game {
+	public SpriteBatch batch;
 
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-//		bat = new ShapeRenderer();
-		sprite = new Sprite(img);
-		sprite.setPosition(
-				Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
-				Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2
-		);
+		this.setScreen(new MainGameScreen(this));
+//		img = new Texture("badlogic.jpg");
+//		sprite = new Sprite(img);
+//		sprite.setPosition(
+//				Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
+//				Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2
+//		);
 	}
 
 	@Override
-	public void render () {
-
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
-			sprite.translateX(-1f);
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-			sprite.translateX(1f);
-
-		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT))
-			sprite.setPosition(Gdx.input.getX(), Gdx.input.getY());
-
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-
-
-
-		batch.begin();
-		batch.draw(sprite, sprite.getX(), sprite.getY());
-		batch.end();
-	}
-
-	@Override
-	public void resize(int width, int height){
-
-	}
-
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		super.render();
+//		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+//			sprite.translateX(-1f);
+//		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+//			sprite.translateX(1f);
+//
+//		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+//			sprite.setPosition( Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+//
+//		Gdx.gl.glClearColor(0, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//
+//
+//
+//
+//		batch.begin();
+//		batch.draw(sprite, sprite.getX(), sprite.getY());
+//		batch.end();
 	}
 }
+//	@Override
+//	public void resize(int width, int height){
+//
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
+//
+//}
---
+++ core/src/com/mygdx/game/screens/MainGameScreen.java
@@ -0,0 +1,74 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.MyGdxGame;
+
+public class MainGameScreen implements Screen {
+    Texture img;
+
+    public static final float SPEED = 120;
+
+    float x;
+    float y;
+
+    MyGdxGame game;
+
+    public MainGameScreen (MyGdxGame game){
+        this.game = game;
+
+    }
+
+    @Override
+    public void show() {
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            x -= SPEED * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            x += SPEED * Gdx.graphics.getDeltaTime();
+
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+
+
+        game.batch.begin();
+        game.batch.draw(img, x, y);
+        game.batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
